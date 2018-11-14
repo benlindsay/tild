f961cdc2da69593f23c0da559cdb3310c5c12485
Ben Lindsay <benjlindsay@gmail.com>
2018-11-14 12:59:15 -0500

    fixed checkout and added flag to turn off gmock and gtest builds for yaml-cpp

--- tools/install-yaml-cpp.sh
+++ tools/install-yaml-cpp.sh
@@ -9,11 +9,11 @@ PROJECT_ROOT=$(git rev-parse --show-toplevel)
 
 rm -rf $YAML_CPP
 git clone https://github.com/jbeder/yaml-cpp $YAML_CPP && \
-  git checkout -b 2018-05-14 4fb1c4b92bf8d9 && \
-  mkdir $YAML_CPP/build && \
-  cd $YAML_CPP/build && \
-  CC=$(which gcc) CXX=$(which g++) \
-    cmake -DCMAKE_INSTALL_PREFIX=$PROJECT_ROOT .. && \
+  cd $YAML_CPP && \
+  git checkout -b 2018-05-14 4fb1c4b92bf8d94b32ebccdd890407d45b3bc794 && \
+  mkdir build && \
+  cd build && \
+  CC=$(which gcc) CXX=$(which g++) cmake -DCMAKE_INSTALL_PREFIX=$PROJECT_ROOT \
+    -DYAML_CPP_BUILD_TESTS=OFF .. && \
   make && \
-  make install && \
-  rm -rf $PROJECT_ROOT/include/gmock  $PROJECT_ROOT/include/gtest
+  make install
