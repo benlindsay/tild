7b4dba4e577d6ffbd598cb56addc435a23114380
Ben Lindsay <benjlindsay@gmail.com>
2018-11-15 16:03:09 -0500

    added lammpstrj writing framework

--- src/output_factory.cpp
+++ src/output_factory.cpp
@@ -3,6 +3,7 @@
 // Copyright (c) 2018 Ben Lindsay <benjlindsay@gmail.com>
 
 #include "output_factory.hpp"
+#include "output_types/lammpstrj_output.hpp"
 #include "output_types/summary_output.hpp"
 
 Output* Output_Factory::New_Output(Sim* sim, YAML::Node input,
@@ -37,8 +38,32 @@ Output* Output_Factory::New_Output(Sim* sim, YAML::Node input,
     fs::path file_path = output_dir / fs::path(file_name);
     return new Summary_Output(sim, var_list, print_freq, file_path,
                               column_width, write_header);
+  } else if (output_type == "lammpstrj") {
+    fs::path output_dir(input["output_dir"].as<std::string>());
+    int print_freq = Output::default_print_freq;
+    std::string name = "trajectory";
+    bool one_frame_per_file = true;
+    for (YAML::const_iterator it = output_type_params.begin();
+         it != output_type_params.end(); ++it) {
+      std::string key = it->first.as<std::string>();
+      YAML::Node value = it->second;
+      if (key == "name") {
+        name = value.as<std::string>();
+      } else if (key == "print_freq") {
+        print_freq = value.as<int>();
+      } else if (key == "one_frame_per_file") {
+        one_frame_per_file = value.as<bool>();
+      } else {
+        utils::die("Can't recognize lammpstrj output parameter '" +
+                   value.as<std::string>() + "'");
+      }
+      return new Lammpstrj_Output(sim, sim->component_list, output_dir,
+                                  print_freq, name, one_frame_per_file);
+    }
   } else {
     utils::die("Can't recognize output type '" + output_type + "'!");
     return NULL;
   }
+  // Suppress "control may reach end of non-void function" warning
+  return NULL;
 }
