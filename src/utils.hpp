38efbf60f2799d4cb52370a7b877d66313522a2a
Ben Lindsay <benjlindsay@gmail.com>
2018-11-24 12:48:39 -0600

    Eigen/Dense in quotes

--- src/utils.hpp
+++ src/utils.hpp
@@ -5,11 +5,11 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
-#include <Eigen/Dense>
 #include <algorithm>  // std::transform
 #include <fstream>    // std::ofstream
 #include <iostream>   // std::cout, std::endl
 #include <string>     // std::string
+#include "Eigen/Dense"
 #include "globals.hpp"
 #include "yaml-cpp/yaml.h"
 
