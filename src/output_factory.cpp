3fb867ccdb4eb789e504b51f89301f3477f575f3
Ben Lindsay <benjlindsay@gmail.com>
2019-01-16 16:47:10 -0500

    removed dummy_bounds option since 3d lammpstrj files should always have 3 box bounds

--- src/output_factory.cpp
+++ src/output_factory.cpp
@@ -39,7 +39,6 @@ Output* Output_Factory::New_Output(Sim* sim, YAML::Node input,
     int print_freq = Output::default_print_freq;
     std::string name = "trajectory";
     bool one_frame_per_file = false;
-    bool dummy_box_bounds = true;
     for (YAML::const_iterator it = output_type_params.begin();
          it != output_type_params.end(); ++it) {
       std::string key = it->first.as<std::string>();
@@ -50,16 +49,13 @@ Output* Output_Factory::New_Output(Sim* sim, YAML::Node input,
         print_freq = value.as<int>();
       } else if (key == "one_frame_per_file") {
         one_frame_per_file = value.as<bool>();
-      } else if (key == "dummy_box_bounds") {
-        dummy_box_bounds = value.as<bool>();
       } else {
         utils::die("Can't recognize lammpstrj output parameter '" +
                    value.as<std::string>() + "'");
       }
     }
     return new Lammpstrj_Output(sim, sim->component_list, output_dir,
-                                print_freq, name, one_frame_per_file,
-                                dummy_box_bounds);
+                                print_freq, name, one_frame_per_file);
   } else if (output_type == "summary") {
     std::vector<std::string> var_list = sim->default_summary_var_list;
     int print_freq = Output::default_print_freq;
--- src/output_types/lammpstrj_output.cpp
+++ src/output_types/lammpstrj_output.cpp
@@ -7,24 +7,20 @@
 Lammpstrj_Output::Lammpstrj_Output(Sim *sim,
                                    std::vector<Component *> component_list,
                                    fs::path output_dir, int print_freq,
-                                   std::string name, bool one_frame_per_file,
-                                   bool dummy_box_bounds)
+                                   std::string name, bool one_frame_per_file)
     : Output(sim) {
-  init(component_list, output_dir, print_freq, name, one_frame_per_file,
-       dummy_box_bounds);
+  init(component_list, output_dir, print_freq, name, one_frame_per_file);
 }
 
 Lammpstrj_Output::~Lammpstrj_Output() { file.close(); }
 
 void Lammpstrj_Output::init(std::vector<Component *> _component_list,
                             fs::path _output_dir, int _print_freq,
-                            std::string _name, bool _one_frame_per_file,
-                            bool _dummy_box_bounds) {
+                            std::string _name, bool _one_frame_per_file) {
   component_list = _component_list;
   print_freq = _print_freq;
   name = _name;
   one_frame_per_file = _one_frame_per_file;
-  dummy_box_bounds = _dummy_box_bounds;
 
   file_path = _output_dir / (name + ".lammpstrj");
   fs::create_directories(file_path.parent_path());
@@ -58,7 +54,7 @@ void Lammpstrj_Output::write() {
     }
     // lammpstrj files need to have 3 box bounds lines even if it is in 2D to be
     // readable by Ovito
-    if (sim->dim == 2 && dummy_box_bounds) {
+    if (sim->dim == 2) {
       file << 0.0 << " " << 0.0 << "\n";
     }
     file << "ITEM: ATOMS id type mol x y";
